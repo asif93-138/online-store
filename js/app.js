f7d4c196325255b355b3740d6fe78bee520125e8
asif93-138 <asif93@student.sust.edu>
2023-08-26 21:28:44 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -13,29 +13,30 @@ loadProducts('https://fakestoreapi.com/products');
 
 // show all product in UI
 const showProducts = (products) => {
-   
+   if (!products[0]) {setInnerText('total_products', 0); document.getElementById("all-products").innerHTML = "<h1>Error!!!</h1>"; return;} 
    setInnerText('total_products', products.length);
 
    document.getElementById("all-products").innerHTML = "";
 
    const allProducts = products.slice(0, 10).map((pd) => pd);
    for (const product of allProducts) {
-      const image = product.images;
+      const image = product.image;
+ 
       const div = document.createElement('div');
       div.classList.add('product');
       div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src="${image}"></img>
       </div>
-      <h3>${product.title}</h3>
+      <h5>${product.title}</h5>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-
+      <h4>Price: $ ${product.price}</h4>
+      <div class="positioning w-100">
       <button onclick="showProductDetails(${product.id})" id="details-btn"    data-bs-toggle="modal"
       data-bs-target="#exampleModal" class="btn btn-outline-secondary mb-2 rounded-1 mt-1">Details</button>
       
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
-      `;
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
+      </div>`;
       document.getElementById('all-products').appendChild(div);
    }
 };
@@ -44,23 +45,25 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
 };
 
 const showProductDetails = (product_id) => {
-   console.log(product_id);
+  
    fetch(`https://fakestoreapi.com/products/${product_id}`)
       .then((res) => res.json())
       .then((data) => showProductDetailsInModal(data));
 };
 
 const showProductDetailsInModal = (product_details) => {
-   console.log(product_details);
+
+   document.getElementById("mod-img").src = product_details.image;
+// setInnerText('', );
    setInnerText('exampleModalLabel', product_details.title);
-   setInnerText('product_id', product_details.id);
+   setInnerText('productId', product_details.id);
    setInnerText('modal_body', product_details.description);
    setInnerText('rating', product_details.rating.rate);
 };
@@ -77,11 +80,12 @@ const updatePrice = (id, value) => {
    const convertPrice = parseInt(value);
    const total = convertedOldPrice + convertPrice;
    document.getElementById(id).innerText = Math.round(total);
+   document.getElementById("total").innerText = Math.round(total) + 20;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-   document.getElementById(id).innerText = Math.round(value);
+   document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
@@ -107,16 +111,17 @@ const updateTotal = () => {
       getInputValue('price') +
       getInputValue('delivery-charge') +
       getInputValue('total-tax');
-   document.getElementById('total').innerText = grandTotal;
+//   document.getElementById('total').innerText = grandTotal;
 };
 
 // search by category
 document.getElementById("search-btn").addEventListener("click", function () {
    const inputField = document.getElementById("input-value").value;
+   const arR = [];
    const searchedProduct = arr[0].find((p) =>
-     p.category.startsWith(`${inputField}`)
-   );
-   showProducts(searchedProduct);
+     p.category.startsWith(inputField)); arR.push(searchedProduct);
+
+   showProducts(arR);
  });
 
 
